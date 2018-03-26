c910ff65a18ed83a6a8e74e6de71c72d3948ff19
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-03-26 19:59:01 +0300

    Add "products" component with "products routing module"

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { PRODUCTS } from './mock-products';
+
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products = PRODUCTS;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
