86f9a8eb5b466e4b5b3735f439b2aac9bac38619
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-03-26 20:43:01 +0300

    Add "mock-products"

---
+++ src/app/products/mock-products.ts
@@ -0,0 +1,15 @@
+
+import {Product} from './product'
+
+export const PRODUCTS: Product[] = [
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null'},
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null'},
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null'},
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name: 'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null' },
+    {code:1010,name:'Apple',category:'cat',description:'description',tax:'0%',price:'10',action:'null'}
+  ];
