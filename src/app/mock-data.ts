6ff22b00d44e394f83567f91deac74329c35f12f
Your Name <you@example.com>
2018-03-27 17:23:44 +0300

    mock data array SalesListItem[ ]

---
+++ src/app/mock-data.ts
@@ -0,0 +1,24 @@
+import { SalesListItem } from './model';
+
+export const SALESITEMS: SalesListItem[] = [
+    { id: 11, number:346 ,custumer:"Walk", tax:8, discount:0, total:108.0 ,	createdBy:"admin Doe" ,totalItems:1, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+    { id: 11, number:1000 ,custumer:"Ali", tax:20, discount:30, total:2000,	createdBy:"Mohammad" ,totalItems:3000, status:"paid" },
+
+  ];
