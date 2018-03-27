b6bd0dd8a01a919c2c6a20626d005c8ccc39f910
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-03-27 20:51:49 +0300

    Implement  the "search bar" functionality

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { PRODUCTS } from './mock-products';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
+  searchText :string;
   products : Product[];
 
   constructor(private spinnerService: Ng4LoadingSpinnerService) { }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { FilterPipe }from './filter.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ProductsRoutingModule
   ],
-  declarations: [ProductsComponent]
+  declarations: [ProductsComponent,FilterPipe]
 })
 export class ProductsModule { }
