3a1dcd01ec1462a73890a7a8adfac58b575b2b96
Your Name <you@example.com>
2018-03-28 13:43:00 +0300

    get sales table by getList( )

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -2,18 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { SalesListItem } from '../model';
 import { SALESITEMS } from '../mock-data';
 
+//declare var hi:any;
+
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent implements OnInit {
-   sales: SalesListItem []= [];
-   salesItems = SALESITEMS ;
-   
-  constructor() { }
+  /*timeoutId = setTimeout(() => {  
+    alert("hi");
+}, 2000);
+*/
+  
+   salesItems : SalesListItem[];
 
+  //s= hi();
+  //salesItem = getSalesList();
+   
+  constructor() {
+   }
+   
   ngOnInit() {
+    this.newMethod();
   }
+  private newMethod() {
+    this.getList();
+  }
+
+   getList(){
+     this.salesItems =   SALESITEMS ;
+   }
 
 }
