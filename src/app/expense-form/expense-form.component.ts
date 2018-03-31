c9d73a905f12e88a0d4b1799f6ea011ed5595ce6
Your Name <you@example.com>
2018-03-31 15:20:27 +0300

    expense-form component files

---
+++ src/app/expense-form/expense-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-form',
+  templateUrl: './expense-form.component.html',
+  styleUrls: ['./expense-form.component.scss']
+})
+export class ExpenseFormComponent implements OnInit {
+  
+
+  constructor() { }
+  
+  ngOnInit() {
+  }
+
+}
