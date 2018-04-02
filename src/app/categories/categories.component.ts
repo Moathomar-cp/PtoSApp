fe2c75183ebe7b10bebbc5b25f30965cf5ecfa41
Your Name <you@example.com>
2018-04-02 20:05:23 +0300

    add "categoriesItems" properity

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CategoriesListItem } from './model';
+import { CATEGORIES } from './mock-data';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categoriesItems: CategoriesListItem[] = [];
 
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
 }
