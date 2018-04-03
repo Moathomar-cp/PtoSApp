d927b3ed01cc6a08b96e80af77ea2dafae8f586a
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-04-03 18:27:54 +0300

    Add "category form" to categories component

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesListItem } from './model';
 import { CATEGORIES } from './mock-data';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { CategoryFormComponent } from '../category-form/category-form.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-categories',
@@ -10,8 +12,9 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 })
 export class CategoriesComponent implements OnInit {
   categoriesItems: CategoriesListItem[] = [];
+  categoryDialogRef: MatDialogRef<CategoryFormComponent>;
 
-  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
+  constructor(private spinnerService: Ng4LoadingSpinnerService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.getCategoriesList();
@@ -25,6 +28,13 @@ export class CategoriesComponent implements OnInit {
       this.categoriesItems = CATEGORIES;
       this.spinnerService.hide();
     }, 1000);
+  }
+
+  openCategoryFormModal() {
+    this.categoryDialogRef = this.dialog.open(CategoryFormComponent, {
+      width: '600px',
+      height: '300px',
+    });
   }
 
 }
