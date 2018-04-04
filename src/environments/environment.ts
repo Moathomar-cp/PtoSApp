5d71ed37dc3a5a7b6f084f55552fe8e992836a87
Your Name <you@example.com>
2018-04-04 12:17:14 +0300

    fixing conflect in enviroment class file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCA55anhCXl6YRYu4-LQZvZ2cOPloHTFxI",
-    authDomain: "testdb-74c48.firebaseapp.com",
-    databaseURL: "https://testdb-74c48.firebaseio.com",
-    projectId: "testdb-74c48",
-    storageBucket: "",
-    messagingSenderId: "284470912203"
-  },
+ 
   version: '(dev)',
   serverUrl: '/api',
   defaultLanguage: 'en-US',
