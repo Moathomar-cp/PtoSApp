ef17f0d774590cf33fe80d263d9289e38fca8a4f
Your Name <you@example.com>
2018-04-08 20:44:51 +0300

    Add "route path" of pos-page to expense page _ to solve problem in pos routing

--- src/app/expenses-list/expenses-list-routing.module.ts
+++ src/app/expenses-list/expenses-list-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExpensesListComponent } from '@app/expenses-list/expenses-list.component';
 import { extract, Route  } from '@app/core';
+import { PosPageComponent } from '@app/pos-page/pos-page.component';
 
 const routes: Routes = [
   Route.withShell([
-    { path: 'expense', component: ExpensesListComponent, data: { title: extract('Expense') } }
+    { path: 'expense', component: ExpensesListComponent, data: { title: extract('Expense') }} ,
+    { path: 'pos', component: PosPageComponent, data: { title: extract('pos') } }
+
   ])
 ];
 
