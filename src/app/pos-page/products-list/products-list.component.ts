144f1bdcfc300c3f78bf184ef5b44bdb7204e479
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-04-10 21:16:26 +0300

    Modify "products list" component to display filtered products

--- src/app/pos-page/products-list/products-list.component.ts
+++ src/app/pos-page/products-list/products-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductsProvider } from '@app/providers/products';
+import { CategoriesProvider } from '@app/providers/categories';
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  @Input() category: string;
+  @Input() filteredProducts: any;
+  @Input () searchText :any;
+  products: any;
 
-  constructor() { }
+
+  constructor(
+    private productsProvider: ProductsProvider,
+    private categoriesProvider: CategoriesProvider) {
+
+  }
 
   ngOnInit() {
+    this.products = this.productsProvider.getList();
   }
+
+  ngOnChanges(category: string) {
+    this.products = this.filteredProducts;
+  }
+
+
 
 }
